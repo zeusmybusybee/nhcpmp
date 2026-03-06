630a2d66b30cc248f5c5eeb12d810f7e053fe2d3
zeusmybusybee <112681252+zeusmybusybee@users.noreply.github.com>
2026-03-06 13:49:10 +0800

    update breadcrumbs and security in books

--- wp-content/themes/memory-child/partials/breadcrumbs.php
+++ wp-content/themes/memory-child/partials/breadcrumbs.php
@@ -1,6 +1,5 @@
 <style>
     .breadcrumbs_container {
-
         font-size: 14px;
     }
 
@@ -28,16 +27,38 @@
         font-size: 10px;
     }
 </style>
-<div class="breadcrumbs_container ">
+
+<div class="breadcrumbs_container">
     <nav class="breadcrumb-nav">
 
         <!-- Home -->
         <a href="<?php echo home_url(); ?>">Home</a>
 
-        <span> <i class="fa-solid fa-angles-right"></i> </span>
+        <span><i class="fa-solid fa-angles-right"></i></span>
 
-        <!-- Archive Title -->
-        <span><?php post_type_archive_title(); ?></span>
+        <?php
+        $post_type = get_post_type();
+        $post_type_obj = get_post_type_object($post_type);
+
+        if (is_post_type_archive()) :
+        ?>
+
+            <!-- Archive Title -->
+            <span><?php post_type_archive_title(); ?></span>
+
+        <?php elseif (is_singular($post_type)) : ?>
+
+            <!-- Archive Link -->
+            <a href="<?php echo get_post_type_archive_link($post_type); ?>">
+                <?php echo $post_type_obj->labels->name; ?>
+            </a>
+
+            <span><i class="fa-solid fa-angles-right"></i></span>
+
+            <!-- Post Title -->
+            <span><?php the_title(); ?></span>
+
+        <?php endif; ?>
 
     </nav>
 </div>
